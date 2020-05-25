59b7ef637363e152f17f24136c97815ded488dc8
belbrun <brunobelic@hotmail.com>
2020-05-26 00:25:04 +0200

    Add rnn model

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 import sklearn
 import numpy as np
+import torch.nn as nn
 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report, f1_score
+
+
 
 class Model():
 
@@ -27,3 +30,24 @@ class Baseline(Model):
 
     def output(self, X):
         return self.svm.predict(X)
+
+
+class RNN(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.activation = nn.ReLU()
+        self.criterion = nn.CrossEntropyLoss()
+        self.lstm = nn.LSTM(input_size=200, hidden_size=300, num_layers=2,
+                            dropout=0, bidirectional=True)
+        self.fc1 = nn.Linear(305, 100)
+        self.fc2 = nn.Linear(100, 20)
+
+    def forward(self, x, data):
+        #preprocess to time first, random initi h0 and c0?
+        _, (h, _) = self.lstm(x)
+        x = torch.cat((x, data), dim=1)
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
+        return x
